00719c003d6f94bc8d773493943f9e102fe4fdee
notoverflow <olivier.radet@alumni.univ-avignon.fr>
2023-10-22 21:09:17 +0200

    finish :)

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,70 @@
 /*   By: oliradet <oliradet@student.42perpignan.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:34:49 by oliradet          #+#    #+#             */
-/*   Updated: 2023/10/17 17:34:49 by oliradet         ###   ########.fr       */
+/*   Updated: 2023/10/22 20:51:17 by oliradet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	count_words(const char *str, char c)
+{
+	int	i;
+	int	trigger;
+
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
+
+static char	*word_dup(const char *str, int start, int finish)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**tab;
-	int		i;
-	int		j;
-	int		n;
+	size_t	i;
+	size_t	j;
+	int		index;
+	char	**split;
 
-	if (!s)
-		return (NULL);
-	tab = malloc(sizeof(char *) * (ft_strlen((char *)s) + 1));
-	if (!tab)
-		return (NULL);
+	(split = malloc((count_words(s, c) + 1) * sizeof(char *)));
+	if (!s || !(split))
+		return (0);
 	i = 0;
 	j = 0;
-	while (s[i])
+	index = -1;
+	while (i <= (unsigned int)ft_strlen(s))
 	{
-		while (s[i] == c)
-			i++;
-		n = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (i > n)
-			tab[j++] = ft_substr(s, n, i - n);
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == (unsigned int)ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = word_dup(s, index, i);
+			index = -1;
+		}
+		i++;
 	}
-	tab[j] = NULL;
-	return (tab);
+	split[j] = 0;
+	return (split);
 }
