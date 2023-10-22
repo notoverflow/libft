10f5254f1ea7c8301778600c93322bfd1f1e3597
notoverflow <olivier.radet@alumni.univ-avignon.fr>
2023-10-22 22:15:41 +0200

    libft finish

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -16,12 +16,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (*lst == NULL)
+	if (!lst || !new)
+		return ;
+	last = ft_lstlast(*lst);
+	if (!last)
 		*lst = new;
 	else
-	{
-		last = ft_lstlast(*lst);
 		last->next = new;
-	}
-	new->next = NULL;
 }
